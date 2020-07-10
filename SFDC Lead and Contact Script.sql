9505c5d83640fe2be34678d48fbdce07337fb12b
cyoung03 <cyoung@wiley.com>
2020-07-10 12:16:03 +0100

    updated contact and leads script
    Please can you review @riedluk

--- SFDC Lead and Contact Script.sql
+++ SFDC Lead and Contact Script.sql
@@ -1,4 +1,5 @@
- select
+select distinct
+
 cm.id as campaign_member_id
 ,cm.campaignid
 ,cm.leadorcontactid
@@ -11,29 +12,22 @@ cm.id as campaign_member_id
 ,cm.LEAD_SOURCE__C
 ,cm.CAMPAIGN_BUSINESS_UNIT__C
 ,c.ULTIMATE_PARENT_ID__C
-,c.CAMPAIGN_NAME Campaign 
+,c.name 
 ,cm.CAMPAIGN_MEMBER_ASSIGNED_DATE__C 
 ,cm.UNQUALIFIED_REASON__C
 ,cm.PRIMARY_PRODUCT_INTEREST__C as Primary_Product
-,u.name as Owner_Name
-,u.title as Owner_Role
+,u.lastname
+,u.title
 
-   from T_S_SFDC_CAMPAINGN_MEM cm
-   join 
-   
-   (select id as campaignid, ULTIMATE_PARENT_ID__C,  name as campaign_name, max(lastmodifieddate) lastmodifieddate
-       
-       from t_s_sfdc_campaign
-       where ULTIMATE_PARENT_ID__C in ('7010W000002efsq','7010W0000023vOE','7010W0000027yf2')
-        group by id, ULTIMATE_PARENT_ID__C, campaign_name
-       ) c on cm.campaignid = c.campaignid
-     
-     left join t_s_sfdc_user u on cm.leadorcontactownerid = u.id
-   
-  where cm.CAMPAIGN_BUSINESS_UNIT__C != 'Research - SM - Society Marketing Strategy'
-    and lower(cm.email) not like '%@wiley.com%'
-    and u.lastname not like '%Test'
-    and (u.title is null or lower(u.title) not like '%marketing%')
-  
-  
-  
+from T_S_SFDC_CAMPAINGN_MEM cm
+left join T_s_sfdc_campaign c on c.id = cm.campaignid
+left join t_s_sfdc_user u on cm.leadorcontactownerid = u.id
+
+
+where  c.ULTIMATE_PARENT_ID__C in ('7010W000002efsq','7010W0000023vOE','7010W0000027yf2')
+and cm.CAMPAIGN_BUSINESS_UNIT__C != 'Research - SM - Society Marketing Strategy'
+and lower(cm.email) not like '%@wiley.com%'
+and cm.lastname not like '%Test'
+--and (u.title is null or (lower(u.title) not like '%marketing%' ) ) 
+
+and cm.CAMPAIGN_MEMBER_ASSIGNED_DATE__C >= '2020-05-01'
