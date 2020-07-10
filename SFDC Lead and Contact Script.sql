218d8633a8f184e900f9a3ab0c0561f1e8eec17a
Chris Riedl <48359770+RiedlUK@users.noreply.github.com>
2020-07-10 15:52:38 +0100

    Updated with ROLE IDs to exclude

--- SFDC Lead and Contact Script.sql
+++ SFDC Lead and Contact Script.sql
@@ -16,18 +16,45 @@ cm.id as campaign_member_id
 ,cm.CAMPAIGN_MEMBER_ASSIGNED_DATE__C 
 ,cm.UNQUALIFIED_REASON__C
 ,cm.PRIMARY_PRODUCT_INTEREST__C as Primary_Product
-,u.lastname
-,u.title
 
 from T_S_SFDC_CAMPAINGN_MEM cm
-left join T_s_sfdc_campaign c on c.id = cm.campaignid
+left join t_s_sfdc_campaign c on c.id = cm.campaignid
 left join t_s_sfdc_user u on cm.leadorcontactownerid = u.id
-
 
 where  c.ULTIMATE_PARENT_ID__C in ('7010W000002efsq','7010W0000023vOE','7010W0000027yf2')
 and cm.CAMPAIGN_BUSINESS_UNIT__C != 'Research - SM - Society Marketing Strategy'
 and lower(cm.email) not like '%@wiley.com%'
 and cm.lastname not like '%Test'
---and (u.title is null or (lower(u.title) not like '%marketing%' ) ) 
-
+and (u.title is null or cm.OWNERID__C != '005d0000002eSQVAA2') -- Eloqua Marketing USER
 and cm.CAMPAIGN_MEMBER_ASSIGNED_DATE__C >= '2020-05-01'
+and (u.USERROLEID is Null or u.USERROLEID not in ('00Ed0000000ieKCEAY' --RES Library Americas Marketing
+                                                ,'00Ed0000000ibBlEAI'--RES Marketing
+                                                ,'00E0W000001qIVfUAM'--RES Corporate Marketing
+                                                ,'00Ed0000000ibBmEAI'--RES Marketing Manager	
+                                                ,'00E0W000001qIVaUAM'--APAC Marketing
+                                                ,'00Ed0000001hh9OEAQ'--Society Marketing
+                                                ,'00E0W0000023QaYUAU'--APAC China Marketing
+                                                ,'00Ed0000000eHPQEA2'--APAC Marketing
+                                                ,'00Ed0000000hY27EAE'--Executive Management
+                                                ))
+
+;
+
+
+---filter on USERROLEID in User table for "Marketing & Executive. Jimmy to send the IDs. 
+--Role	15digitid	18digitid
+--RES Library Americas Marketing	00Ed0000000ieKC	00Ed0000000ieKCEAY
+--RES Marketing	00Ed0000000ibBl	00Ed0000000ibBlEAI
+--RES Corporate Marketing	00E0W000001qIVf	00E0W000001qIVfUAM
+--APAC Marketing	00E0W000001qIVa	00E0W000001qIVaUAM
+--RES Marketing Manager	00Ed0000000ibBm	00Ed0000000ibBmEAI
+--Society Marketing	00Ed0000001hh9O	00Ed0000001hh9OEAQ
+--APAC China Marketing	00E0W0000023QaY	00E0W0000023QaYUAU
+--APAC Marketing	00Ed0000000eHPQ	00Ed0000000eHPQEA2
+--Executive Management	00Ed0000000hY27	00Ed0000000hY27EAE
+
+
+select *
+
+from T_S_SFDC_CAMPAINGN_MEM
+//where ID = '005d0000002eSQVAA2'
